5bd7f21be1816cd0c753c3223b0955774dd877ea
BRUTEFORCE-HACKER <84610202+BRUTEFORCE-HACKER@users.noreply.github.com>
2024-01-25 17:10:16 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import os ,sys ,hashlib ,json ,random ,re ,time ,subprocess #line:1
+localtime =time .asctime (time .localtime (time .time ()))#line:3
+def restart_program ():#line:5
+	OOO0OOO00O00OO000 =sys .executable #line:6
+	os .execl (OOO0OOO00O00OO000 ,OOO0OOO00O00OO000 ,*sys .argv )#line:7
+def clear ():#line:9
+       if os .name =='nt':#line:10
+                  _OO0OOOOOO0OO0O0OO =os .system ('cls')#line:11
+       else :#line:12
+                  _OO0OOOOOO0OO0O0OO =os .system ('clear')#line:13
+       if os .name =='nt':#line:14
+           _OO0OOOOOO0OO0O0OO =os .system ('cls')#line:15
+       else :#line:16
+           _OO0OOOOOO0OO0O0OO =os .system ('clear')#line:17
+time .sleep (2 )#line:19
+print ("\n\033[92m Tool is running! Please wait! \033[92m")#line:20
+time .sleep (3 )#line:21
+clear ()#line:22
+print ('\033[93m DATE : \033[96m',localtime )#line:23
+print ('''\033[96m
+
+████████╗ ██████╗ ██████╗     
+╚══██╔══╝██╔════╝██╔════╝     
+   ██║   ██║     ██║  ███╗    
+   ██║   ██║     ██║   ██║    
+   ██║   ╚██████╗╚██████╔╝    
+   ╚═╝    ╚═════╝ ╚═════╝     
+                              
+\033[92m ----------------------------------------------\n Official social media accounts below: \n ----------------------------------------------
+\033[0m\n''')#line:34
+time .sleep (3 )#line:36
+print ("\033[93m1:    Main Facebook Account\033[0m")#line:38
+print ("\033[93m2:    Official Seller Account\033[0m\n")#line:39
+print ("\033[95mNOTE:  Use CTRL + z to stop this program\033[0m")#line:40
+time .sleep (1 )#line:41
+choice =input ("\033[96mEnter your choice:  \033[0m")#line:42
+if choice =='1':#line:44
+	print ("\n\033[96mComendador Joel \033[97m (With 👩‍💻 and verified emoji)\033[0m")#line:45
+elif choice =='2':#line:46
+	print ("\n\033[96mJJ Flyvio \033[97m(Creating)\033[0m")#line:47
+time .sleep (5 )#line:50
+clear ()#line:51
+time .sleep (1 )#line:52
+print ("\n\n\033[95mThank you for visiting this tool!\033[0m")#line:53
+time .sleep (1 )#line:54
+print ("\033[95mRestarting in 3 sec..\033[0m")#line:55
+time .sleep (3 )#line:56
+restart_program ()
